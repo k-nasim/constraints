8281e60663dc50a2c89f264e915412ccaf62452f
k-nasim <130048247+k-nasim@users.noreply.github.com>
2023-07-25 15:24:12 +0530

    Update ac_glitch.v

--- ac_glitch.v
+++ ac_glitch.v
@@ -1,6 +1,7 @@
 module top( input in1,in2,clk1,clk2,rst1,rst2, output reg out);
 
 reg a,b;
+reg sync;
 wire AND;
 always @( posedge clk1 or posedge rst1) begin
     if(rst1)
@@ -15,7 +16,9 @@ assign AND = a^b;
 
 always @(posedge clk2 or posedge rst2)
     if(rst2)
-        out <= 0;
-    else
-        out <= AND;
+        {sync,out} <= 0;
+    else begin
+        sync <= AND;
+		  out <= sync;
+		 end
 endmodule
